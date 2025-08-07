6987342c9c795e90a7167638362e562b4a34319f
Rafael Costa <rafaelcosta8020@gmail.com>
2025-08-07 08:43:42 -0300

    2. Testes E2E de Criação de Usuário (Guest)

--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,9 +1,18 @@
+import { v4 as uuidv4 } from "uuid";
 import { User } from "../../domain/entities/user";
 import { UserRepository } from "../../domain/repositories/user_repository";
+import { CreateUserDTO } from "../dtos/create_user_dto";
 
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
   async findUserById(id: string): Promise<User | null> {
     return this.userRepository.findById(id);
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<User> {
+    const user = new User(uuidv4(), dto.name);
+    await this.userRepository.save(user);
+
+    return user;
   }
 }
